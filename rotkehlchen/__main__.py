d2c48faf6b937757f477792c637cab8b708753d1
collateralusr <110911796+collateralusr@users.noreply.github.com>
2023-10-23 16:54:33 -0400

    Update __main__.py
    possible simplified the code

--- rotkehlchen/__main__.py
+++ rotkehlchen/__main__.py
@@ -1,5 +1,3 @@
-from gevent import monkey  # isort:skip
-monkey.patch_all()  # isort:skip
 import logging
 import sys
 import traceback
@@ -8,27 +6,13 @@ from rotkehlchen.errors.misc import DBSchemaError, SystemPermissionError
 from rotkehlchen.logging import RotkehlchenLogsAdapter
 from rotkehlchen.server import RotkehlchenServer
 
-logger = logging.getLogger(__name__)
-log = RotkehlchenLogsAdapter(logger)
-
-
 def main() -> None:
     try:
         rotkehlchen_server = RotkehlchenServer()
-    except (SystemPermissionError, DBSchemaError) as e:
+    except (SystemPermissionError, DBSchemaError, SystemExit) as e:
         print(f'ERROR at initialization: {e!s}')
-        sys.exit(1)
-    except SystemExit as e:
-        if e.code is None or e.code == 0 or e.code == 2:
-            # exit_code 2 is for invalid arguments
-            exit_code = 0 if e.code is None else e.code
-            sys.exit(exit_code)
-        else:
-            tb = traceback.format_exc()
-            logging.critical(tb)
-            print(f'Failed to start rotki backend:\n{tb}')
-            sys.exit(1)
-    except:  # noqa: B001, E722, RUF100  # pylint: disable=bare-except
+        sys.exit(get_exit_code(e))
+    except Exception:
         tb = traceback.format_exc()
         logging.critical(tb)
         print(f'Failed to start rotki backend:\n{tb}')
@@ -36,6 +20,8 @@ def main() -> None:
 
     rotkehlchen_server.main()
 
+def get_exit_code(e):
+    return e.code if e.code is not None and e.code in {0, 2} else 1
 
 if __name__ == '__main__':
     main()
